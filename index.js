7643db70b6d6c63af6d8261cf8677eb99fedc6ea
Hriday Mehra <hridaymehra3@gmail.com>
2024-03-30 11:04:34 +0530

    Final Commit

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ const engine = Engine.create();
 engine.world.gravity.y = 0;
 const {world} = engine;
 
-const width = 600, height = 600;
-const cells = 3;
-const unitLength = width / cells;
+// Configuration Variables for Maze
+const width = window.innerWidth, height = window.innerHeight;
+const cellsHorizontal = 10, cellsVertical = 7;
+const unitLengthX = width / cellsHorizontal;
+const unitLengthY = height / cellsVertical;
 
 const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
         width: width,
-        height: height
+        height: height,
+        wireframes: false
     }
 });
 
@@ -23,10 +26,10 @@ Runner.run(Runner.create(), engine);
 
 // Walls or Borders of Canvas
 const walls = [
-    Bodies.rectangle(width/2, 0, width, 3, {isStatic: true}),
-    Bodies.rectangle(width/2, height, height, 3, {isStatic: true}),
-    Bodies.rectangle(0, height/2, 3, height, {isStatic: true}),
-    Bodies.rectangle(width, height/2, 3, height, {isStatic: true})
+    Bodies.rectangle(width/2, 0, width, 10, {isStatic: true}),
+    Bodies.rectangle(width/2, height, height, 10, {isStatic: true}),
+    Bodies.rectangle(0, height/2, 10, height, {isStatic: true}),
+    Bodies.rectangle(width, height/2, 10, height, {isStatic: true})
 ];
 
 World.add(world, walls);
@@ -46,20 +49,20 @@ const shuffleAndMakeRandom = (arr) => {
     return arr;
 };
 
-const grid = Array(cells)
+const grid = Array(cellsVertical)
     .fill(null)
-    .map(() => Array(cells).fill(false));
+    .map(() => Array(cellsHorizontal).fill(false));
 
-const verticals = Array(cells)
+const verticals = Array(cellsVertical)
     .fill(null)
-    .map(() => Array(cells-1).fill(false));
+    .map(() => Array(cellsHorizontal-1).fill(false));
 
-const horizontals = Array(cells-1)
+const horizontals = Array(cellsVertical-1)
     .fill(null)
-    .map(() => Array(cells).fill(false));
+    .map(() => Array(cellsHorizontal).fill(false));
 
-const startRow = Math.floor(Math.random() * cells);
-const startCol = Math.floor(Math.random() * cells);
+const startRow = Math.floor(Math.random() * cellsVertical);
+const startCol = Math.floor(Math.random() * cellsHorizontal);
 
 const makeHorizontalSegments = () => {
     horizontals.forEach((row, rowIndex) => {
@@ -68,11 +71,15 @@ const makeHorizontalSegments = () => {
                 return;
 
             const wall = Bodies.rectangle(
-                colIndex * unitLength + unitLength/2,
-                rowIndex * unitLength + unitLength,
-                unitLength,
+                colIndex * unitLengthX + unitLengthX/2,
+                rowIndex * unitLengthY + unitLengthY,
+                unitLengthX,
                 10,
-                { isStatic: true }
+                { 
+                    isStatic: true,
+                    label: 'wall',
+                    render: { fillStyle: 'red' }
+                }
             );            
             World.add(world, wall);
         });
@@ -86,11 +93,15 @@ const makeVerticalSegments = () => {
                 return;
 
             const wall = Bodies.rectangle(
-                colIndex * unitLength + unitLength,
-                rowIndex * unitLength + unitLength/2,
+                colIndex * unitLengthX + unitLengthX,
+                rowIndex * unitLengthY + unitLengthY/2,
                 10,
-                unitLength,
-                { isStatic: true }
+                unitLengthY,
+                { 
+                    isStatic: true,
+                    label: 'wall',
+                    render: { fillStyle: 'red' }
+                }
             );
             World.add(world, wall);
         });
@@ -114,7 +125,7 @@ const mazeGenerator = (row, column) => {
     for(let neighbor of neighbors)
     {
         const [nextRow, nextCol, direction] = neighbor;
-        if(nextRow < 0 || nextRow >= cells || nextCol < 0 || nextCol >= cells)
+        if(nextRow < 0 || nextRow >= cellsVertical || nextCol < 0 || nextCol >= cellsHorizontal)
             continue;
         
         if(grid[nextRow][nextCol])
@@ -140,19 +151,20 @@ const mazeGenerator = (row, column) => {
 
 // Goal 
 const goal = Bodies.rectangle(
-    width - unitLength/2,
-    height - unitLength/2,
-    unitLength * 0.7,
-    unitLength * 0.7,
-    { label: 'goal', isStatic: true }
+    width - unitLengthX/2,
+    height - unitLengthY/2,
+    unitLengthX * 0.7,
+    unitLengthY * 0.7,
+    { label: 'goal', isStatic: true, render: { fillStyle: 'green' } }
 );
 
 // Ball
+const ballRadius = Math.min(unitLengthX, unitLengthY) / 4;
 const ball = Bodies.circle(
-    unitLength/2,
-    unitLength/2,
-    unitLength/4,
-    { label: 'ball' }
+    unitLengthX/2,
+    unitLengthY/2,
+    ballRadius,
+    { label: 'ball', render: { fillStyle: 'blue' } }
 );
 World.add(world, goal);
 World.add(world, ball);
@@ -182,13 +194,22 @@ document.addEventListener('keydown', (event) => {
 
 });
 
+const userWonGame = () => {
+    world.gravity.y = 1;
+    world.bodies.forEach((body) => {
+        if(body.label === 'wall')
+            Body.setStatic(body, false);
+    });
+    document.querySelector('.winner').classList.remove('hidden');
+};
+
 // Win Condition
 Events.on(engine, 'collisionStart', event => {
     event.pairs.forEach(collision => {
         const labels = ['ball', 'goal'];
         if(labels.includes(collision.bodyA.label) && labels.includes(collision.bodyB.label))
         {
-            console.log("User Won");
+            userWonGame();
         }
     });
 });
